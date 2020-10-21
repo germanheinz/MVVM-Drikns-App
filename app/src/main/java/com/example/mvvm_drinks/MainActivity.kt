7e03653684b11fa544b5d8d8e81e84de319ba52c
germanheinz <heinz.german@gmail.com>
2020-10-21 22:14:08 +0200

    BASIC NAV COMPLETED -

--- app/src/main/java/com/example/mvvm_drinks/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_drinks/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.mvvm_drinks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
+// TODO - NAV - 1 - CREATE IN RES A NEW "ANDROID RESOURCE FILE" WITH VALUE NAVIGATION
+// TODO - NAV - 2 - CREATE THE FRAGMENT TO NAVIGATE BETWEEN THEM
+// TODO - NAV - 3 - ADD IN ACTIVITY MAIN A FRAGMENTCONTAINERVIEW
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO - NAV - 7 - OVERRIDE THIS METHOD AND DEFINE NAVCONTROLLER
+        navController = findNavController(R.id.nav_host_fragment)
+        // BACK ARROW
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+    // TODO - NAV - 6 - OVERRIDE THIS METHOD
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp()
     }
 }
